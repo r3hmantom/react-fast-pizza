471eaf992998f93fca00edf98736b9c98fe11761
r3hmantom <r3hmantom@gmail.com>
2024-01-13 13:11:03 +0500

    Implementing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./ui";
+import { Menu } from "./features/menu/";
+import { Cart } from "./features/cart";
+import { CreateOrder, Order } from "./features/order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
