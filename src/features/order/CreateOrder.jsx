d2deb50ea5f0dbb2dc29f58c325b354bb5899de6
r3hmantom <r3hmantom@gmail.com>
2024-01-14 17:03:51 +0500

    Styling buttons

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -77,7 +77,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" value={JSON.stringify(cart)} name="cart" />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="hover:b-yellow-300 inline-block rounded-full  bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
