86db03d349ac9f4be4c6a0d5ed049e98a4ef0004
r3hmantom <r3hmantom@gmail.com>
2024-01-16 14:48:58 +0500

    Fixed borders

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -29,11 +29,11 @@ function Order() {
 
         <div className="space-x-2">
           {priority && (
-            <span className="bg-red-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-red-50">
+            <span className="rounded-full bg-red-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-red-50">
               Priority
             </span>
           )}
-          <span className="bg-green-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-green-50">
+          <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-green-50">
             {status} order
           </span>
         </div>
