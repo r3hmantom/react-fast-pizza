a8c88701b31494a97e3a966163bac079b8597555
r3hmantom <r3hmantom@gmail.com>
2024-01-17 03:20:20 +0500

    Reading and updating the user state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation, useSearchParams } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,6 +39,7 @@ function CreateOrder() {
   const cart = fakeCart;
   const formErrors = useActionData();
   const navigation = useNavigation();
+  const username = useSelector(state => state.user.username);
   const isSubmitting = navigation.state === "submitting";
 
   return (
@@ -47,7 +49,7 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5  flex  flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input className="input grow" defaultValue={username} type="text" name="customer" required />
         </div>
 
         <div className="mb-5  flex  flex-col gap-2 sm:flex-row sm:items-center">
