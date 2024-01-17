c1e93a8fdf1412ea12655f813dcccf39584ccb74
r3hmantom <r3hmantom@gmail.com>
2024-01-17 12:57:02 +0500

    Adding menu items to the cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -31,7 +31,7 @@ const cartSlice = createSlice({
   },
 });
 
-const {
+export const {
   addItem,
   deleteItem,
   increaseItemQuantity,
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled, to, type }) {
+export default function Button({ children, disabled, to, type, onClick }) {
   const base =
     "hover:bg-yellow-300 focus:bg-yellow-300 inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-sm text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
@@ -17,6 +17,14 @@ export default function Button({ children, disabled, to, type }) {
         {children}{" "}
       </Link>
     );
+
+  if (onClick)
+    return (
+      <button disabled={disabled} className={styles[type]} onClick={onClick}>
+        {children}
+      </button>
+    );
+
   return (
     <button disabled={disabled} className={styles[type]}>
       {children}
